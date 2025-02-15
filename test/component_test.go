0a4b91ed72eb68ddc4bf9d19a9ab7df94e8df1b2
Igor Rodionov <goruha@gmail.com>
2025-02-15 17:58:01 +0100

    Added tests helpers

--- test/component_test.go
+++ test/component_test.go
@@ -7,117 +7,120 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/cloudtrail"
 	"github.com/cloudposse/test-helpers/pkg/atmos"
-	helper "github.com/cloudposse/test-helpers/pkg/atmos/aws-component-helper"
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
+	awshelper "github.com/cloudposse/test-helpers/pkg/aws"
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestComponent(t *testing.T) {
-	t.Parallel()
-	// Define the AWS region to use for the tests
-	awsRegion := "us-east-2"
+type ComponentSuite struct {
+	helper.TestSuite
+}
 
-	// Initialize the test fixture
-	fixture := helper.NewFixture(t, "../", awsRegion, "test/fixtures")
+func (s *ComponentSuite) TestBasic() {
+	const component = "cloudtrail/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-	// Ensure teardown is executed after the test
-	defer fixture.TearDown()
-	fixture.SetUp(&atmos.Options{})
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	assert.NotNil(s.T(), options)
 
-	// Define the test suite
-	fixture.Suite("default", func(t *testing.T, suite *helper.Suite) {
-		t.Parallel()
-		suite.AddDependency("cloudtrail-bucket", "default-test")
+	cloudtrailBucketOptions := s.GetAtmosOptions("cloudtrail-bucket", stack, nil)
+	bucketName := atmos.Output(s.T(), cloudtrailBucketOptions, "cloudtrail_bucket_id")
+	defer aws.EmptyS3Bucket(s.T(), awsRegion, bucketName)
 
-		// Test phase: Validate the functionality of the bastion component
-		suite.Test(t, "basic", func(t *testing.T, atm *helper.Atmos) {
-			defer atm.GetAndDestroy("cloudtrail/basic", "default-test", map[string]interface{}{})
-			component := atm.GetAndDeploy("cloudtrail/basic", "default-test", map[string]interface{}{})
-			assert.NotNil(t, component)
+	cloudtrailID := atmos.Output(s.T(), options, "cloudtrail_id")
 
-			cloudtrailID := atm.Output(component, "cloudtrail_id")
-
-			client := NewCloudTrailClient(t, awsRegion)
-			trails, err := client.DescribeTrails(context.Background(), &cloudtrail.DescribeTrailsInput{
-				TrailNameList: []string{cloudtrailID},
-			})
-			assert.NoError(t, err)
-			trail := trails.TrailList[0]
-
-			cloudtrailArn := atm.Output(component, "cloudtrail_arn")
-			assert.Equal(t, cloudtrailArn, *trail.TrailARN)
-
-			cloudtrailLogsLogGroupArn := atm.Output(component, "cloudtrail_logs_log_group_arn")
-			assert.True(t, strings.HasPrefix(*trail.CloudWatchLogsLogGroupArn, cloudtrailLogsLogGroupArn))
-
-			cloudtrailLogsLogGroupName := atm.Output(component, "cloudtrail_logs_log_group_name")
-			assert.True(t, strings.HasSuffix(cloudtrailLogsLogGroupArn, cloudtrailLogsLogGroupName))
-
-			cloudtrailLogsRoleArn := atm.Output(component, "cloudtrail_logs_role_arn")
-			assert.Equal(t, cloudtrailLogsRoleArn, *trail.CloudWatchLogsRoleArn)
-
-			cloudtrailLogsRoleName := atm.Output(component, "cloudtrail_logs_role_name")
-			assert.True(t, strings.HasSuffix(cloudtrailLogsRoleArn, cloudtrailLogsRoleName))
-
-			cloudtrailHomeRegion := atm.Output(component, "cloudtrail_home_region")
-			assert.Equal(t, "us-east-2", cloudtrailHomeRegion)
-			assert.Equal(t, *trail.HomeRegion, cloudtrailHomeRegion)
-
-			assert.False(t, *trail.IsOrganizationTrail)
-		})
-
-		suite.Test(t, "org-level", func(t *testing.T, atm *helper.Atmos) {
-			t.Skip("Skipping org-level test because it's not supported due to Service Policy limitations")
-			defer atm.GetAndDestroy("cloudtrail/org-level", "default-test", map[string]interface{}{})
-			component := atm.GetAndDeploy("cloudtrail/org-level", "default-test", map[string]interface{}{})
-			assert.NotNil(t, component)
-
-			cloudtrailID := atm.Output(component, "cloudtrail_id")
-
-			client := NewCloudTrailClient(t, awsRegion)
-			trails, err := client.DescribeTrails(context.Background(), &cloudtrail.DescribeTrailsInput{
-				TrailNameList: []string{cloudtrailID},
-			})
-			assert.NoError(t, err)
-			trail := trails.TrailList[0]
-
-			cloudtrailArn := atm.Output(component, "cloudtrail_arn")
-			assert.Equal(t, cloudtrailArn, *trail.TrailARN)
-
-			cloudtrailLogsLogGroupArn := atm.Output(component, "cloudtrail_logs_log_group_arn")
-			assert.True(t, strings.HasPrefix(*trail.CloudWatchLogsLogGroupArn, cloudtrailLogsLogGroupArn))
-
-			cloudtrailLogsLogGroupName := atm.Output(component, "cloudtrail_logs_log_group_name")
-			assert.True(t, strings.HasSuffix(cloudtrailLogsLogGroupArn, cloudtrailLogsLogGroupName))
-
-			cloudtrailLogsRoleArn := atm.Output(component, "cloudtrail_logs_role_arn")
-			assert.Equal(t, cloudtrailLogsRoleArn, *trail.CloudWatchLogsRoleArn)
-
-			cloudtrailLogsRoleName := atm.Output(component, "cloudtrail_logs_role_name")
-			assert.True(t, strings.HasSuffix(cloudtrailLogsRoleArn, cloudtrailLogsRoleName))
-
-			cloudtrailHomeRegion := atm.Output(component, "cloudtrail_home_region")
-			assert.Equal(t, "us-east-2", cloudtrailHomeRegion)
-			assert.Equal(t, *trail.HomeRegion, cloudtrailHomeRegion)
-
-			assert.True(t, *trail.IsOrganizationTrail)
-		})
-
+	client := awshelper.NewCloudTrailClient(s.T(), awsRegion)
+	trails, err := client.DescribeTrails(context.Background(), &cloudtrail.DescribeTrailsInput{
+		TrailNameList: []string{cloudtrailID},
 	})
+	assert.NoError(s.T(), err)
+	trail := trails.TrailList[0]
+
+	cloudtrailArn := atmos.Output(s.T(), options, "cloudtrail_arn")
+	assert.Equal(s.T(), cloudtrailArn, *trail.TrailARN)
+
+	cloudtrailLogsLogGroupArn := atmos.Output(s.T(), options, "cloudtrail_logs_log_group_arn")
+	assert.True(s.T(), strings.HasPrefix(*trail.CloudWatchLogsLogGroupArn, cloudtrailLogsLogGroupArn))
+
+	cloudtrailLogsLogGroupName := atmos.Output(s.T(), options, "cloudtrail_logs_log_group_name")
+	assert.True(s.T(), strings.HasSuffix(cloudtrailLogsLogGroupArn, cloudtrailLogsLogGroupName))
+
+	cloudtrailLogsRoleArn := atmos.Output(s.T(), options, "cloudtrail_logs_role_arn")
+	assert.Equal(s.T(), cloudtrailLogsRoleArn, *trail.CloudWatchLogsRoleArn)
+
+	cloudtrailLogsRoleName := atmos.Output(s.T(), options, "cloudtrail_logs_role_name")
+	assert.True(s.T(), strings.HasSuffix(cloudtrailLogsRoleArn, cloudtrailLogsRoleName))
+
+	cloudtrailHomeRegion := atmos.Output(s.T(), options, "cloudtrail_home_region")
+	assert.Equal(s.T(), "us-east-2", cloudtrailHomeRegion)
+	assert.Equal(s.T(), *trail.HomeRegion, cloudtrailHomeRegion)
+
+	assert.False(s.T(), *trail.IsOrganizationTrail)
+
+	s.DriftTest(component, stack, nil)
 }
 
-func NewCloudTrailClient(t *testing.T, region string) *cloudtrail.Client {
-	client, err := NewCloudTrailClientE(t, region)
-	require.NoError(t, err)
+func (s *ComponentSuite) TestOrgLevel() {
+	const component = "cloudtrail/org-level"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-	return client
+	s.T().Skip("Skipping org-level test because it's not supported due to Service Policy limitations")
+	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	assert.NotNil(s.T(), options)
+
+	cloudtrailBucketOptions := s.GetAtmosOptions("cloudtrail-bucket", stack, nil)
+	bucketName := atmos.Output(s.T(), cloudtrailBucketOptions, "cloudtrail_bucket_id")
+	defer aws.EmptyS3Bucket(s.T(), awsRegion, bucketName)
+
+	cloudtrailID := atmos.Output(s.T(), options, "cloudtrail_id")
+
+	client := awshelper.NewCloudTrailClient(s.T(), awsRegion)
+	trails, err := client.DescribeTrails(context.Background(), &cloudtrail.DescribeTrailsInput{
+		TrailNameList: []string{cloudtrailID},
+	})
+	assert.NoError(s.T(), err)
+	trail := trails.TrailList[0]
+
+	cloudtrailArn := atmos.Output(s.T(), options, "cloudtrail_arn")
+	assert.Equal(s.T(), cloudtrailArn, *trail.TrailARN)
+
+	cloudtrailLogsLogGroupArn := atmos.Output(s.T(), options, "cloudtrail_logs_log_group_arn")
+	assert.True(s.T(), strings.HasPrefix(*trail.CloudWatchLogsLogGroupArn, cloudtrailLogsLogGroupArn))
+
+	cloudtrailLogsLogGroupName := atmos.Output(s.T(), options, "cloudtrail_logs_log_group_name")
+	assert.True(s.T(), strings.HasSuffix(cloudtrailLogsLogGroupArn, cloudtrailLogsLogGroupName))
+
+	cloudtrailLogsRoleArn := atmos.Output(s.T(), options, "cloudtrail_logs_role_arn")
+	assert.Equal(s.T(), cloudtrailLogsRoleArn, *trail.CloudWatchLogsRoleArn)
+
+	cloudtrailLogsRoleName := atmos.Output(s.T(), options, "cloudtrail_logs_role_name")
+	assert.True(s.T(), strings.HasSuffix(cloudtrailLogsRoleArn, cloudtrailLogsRoleName))
+
+	cloudtrailHomeRegion := atmos.Output(s.T(), options, "cloudtrail_home_region")
+	assert.Equal(s.T(), "us-east-2", cloudtrailHomeRegion)
+	assert.Equal(s.T(), *trail.HomeRegion, cloudtrailHomeRegion)
+
+	assert.True(s.T(), *trail.IsOrganizationTrail)
+
+	s.DriftTest(component, stack, nil)
 }
 
-func NewCloudTrailClientE(t *testing.T, region string) (*cloudtrail.Client, error) {
-	sess, err := aws.NewAuthenticatedSession(region)
-	if err != nil {
-		return nil, err
-	}
-	return cloudtrail.NewFromConfig(*sess), nil
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "cloudtrail/disabled"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	s.VerifyEnabledFlag(component, stack, nil)
+}
+
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+
+	suite.AddDependency(t, "cloudtrail-bucket", "default-test", nil)
+	helper.Run(t, suite)
 }
