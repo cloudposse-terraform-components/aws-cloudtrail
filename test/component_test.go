370a18a47e0469525983e9b85917558ef919135d
milldr <miller0daniel@gmail.com>
2025-12-08 11:46:00 -0500

    fix: add TearDownSuite to empty S3 bucket before dependency destroy

--- test/component_test.go
+++ test/component_test.go
@@ -17,22 +17,30 @@ type ComponentSuite struct {
 	helper.TestSuite
 }
 
+const (
+	testStack    = "default-test"
+	testAwsRegion = "us-east-2"
+)
+
+// TearDownSuite empties the S3 bucket before the dependency is destroyed.
+// This is necessary because the bucket has versioning enabled and may contain
+// objects from CloudTrail logging during the tests.
+func (s *ComponentSuite) TearDownSuite() {
+	cloudtrailBucketOptions := s.GetAtmosOptions("cloudtrail-bucket", testStack, nil)
+	bucketName := atmos.Output(s.T(), cloudtrailBucketOptions, "cloudtrail_bucket_id")
+	aws.EmptyS3Bucket(s.T(), testAwsRegion, bucketName)
+}
+
 func (s *ComponentSuite) TestBasic() {
 	const component = "cloudtrail/basic"
-	const stack = "default-test"
-	const awsRegion = "us-east-2"
 
-	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
-	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	defer s.DestroyAtmosComponent(s.T(), component, testStack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, testStack, nil)
 	assert.NotNil(s.T(), options)
-
-	cloudtrailBucketOptions := s.GetAtmosOptions("cloudtrail-bucket", stack, nil)
-	bucketName := atmos.Output(s.T(), cloudtrailBucketOptions, "cloudtrail_bucket_id")
-	defer aws.EmptyS3Bucket(s.T(), awsRegion, bucketName)
 
 	cloudtrailID := atmos.Output(s.T(), options, "cloudtrail_id")
 
-	client := awshelper.NewCloudTrailClient(s.T(), awsRegion)
+	client := awshelper.NewCloudTrailClient(s.T(), testAwsRegion)
 	trails, err := client.DescribeTrails(context.Background(), &cloudtrail.DescribeTrailsInput{
 		TrailNameList: []string{cloudtrailID},
 	})
@@ -55,31 +63,25 @@ func (s *ComponentSuite) TestBasic() {
 	assert.True(s.T(), strings.HasSuffix(cloudtrailLogsRoleArn, cloudtrailLogsRoleName))
 
 	cloudtrailHomeRegion := atmos.Output(s.T(), options, "cloudtrail_home_region")
-	assert.Equal(s.T(), "us-east-2", cloudtrailHomeRegion)
+	assert.Equal(s.T(), testAwsRegion, cloudtrailHomeRegion)
 	assert.Equal(s.T(), *trail.HomeRegion, cloudtrailHomeRegion)
 
 	assert.False(s.T(), *trail.IsOrganizationTrail)
 
-	s.DriftTest(component, stack, nil)
+	s.DriftTest(component, testStack, nil)
 }
 
 func (s *ComponentSuite) TestOrgLevel() {
 	const component = "cloudtrail/org-level"
-	const stack = "default-test"
-	const awsRegion = "us-east-2"
 
 	s.T().Skip("Skipping org-level test because it's not supported due to Service Policy limitations")
-	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
-	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+	defer s.DestroyAtmosComponent(s.T(), component, testStack, nil)
+	options, _ := s.DeployAtmosComponent(s.T(), component, testStack, nil)
 	assert.NotNil(s.T(), options)
-
-	cloudtrailBucketOptions := s.GetAtmosOptions("cloudtrail-bucket", stack, nil)
-	bucketName := atmos.Output(s.T(), cloudtrailBucketOptions, "cloudtrail_bucket_id")
-	defer aws.EmptyS3Bucket(s.T(), awsRegion, bucketName)
 
 	cloudtrailID := atmos.Output(s.T(), options, "cloudtrail_id")
 
-	client := awshelper.NewCloudTrailClient(s.T(), awsRegion)
+	client := awshelper.NewCloudTrailClient(s.T(), testAwsRegion)
 	trails, err := client.DescribeTrails(context.Background(), &cloudtrail.DescribeTrailsInput{
 		TrailNameList: []string{cloudtrailID},
 	})
@@ -102,24 +104,23 @@ func (s *ComponentSuite) TestOrgLevel() {
 	assert.True(s.T(), strings.HasSuffix(cloudtrailLogsRoleArn, cloudtrailLogsRoleName))
 
 	cloudtrailHomeRegion := atmos.Output(s.T(), options, "cloudtrail_home_region")
-	assert.Equal(s.T(), "us-east-2", cloudtrailHomeRegion)
+	assert.Equal(s.T(), testAwsRegion, cloudtrailHomeRegion)
 	assert.Equal(s.T(), *trail.HomeRegion, cloudtrailHomeRegion)
 
 	assert.True(s.T(), *trail.IsOrganizationTrail)
 
-	s.DriftTest(component, stack, nil)
+	s.DriftTest(component, testStack, nil)
 }
 
 func (s *ComponentSuite) TestEnabledFlag() {
 	const component = "cloudtrail/disabled"
-	const stack = "default-test"
 
-	s.VerifyEnabledFlag(component, stack, nil)
+	s.VerifyEnabledFlag(component, testStack, nil)
 }
 
 func TestRunSuite(t *testing.T) {
 	suite := new(ComponentSuite)
 
-	suite.AddDependency(t, "cloudtrail-bucket", "default-test", nil)
+	suite.AddDependency(t, "cloudtrail-bucket", testStack, nil)
 	helper.Run(t, suite)
 }
